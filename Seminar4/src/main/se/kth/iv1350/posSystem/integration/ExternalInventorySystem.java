3ca06839b18af648ae59bd8b992171039913398f
Azmeer K <aaak@kth.se>
2022-05-13 10:30:17 +0200

    Added code for throwing exceptions. Item has been added to item inventory and item catalogue, which will be used for simulating connection issues with external systems.

--- Seminar4/src/main/se/kth/iv1350/posSystem/integration/ExternalInventorySystem.java
+++ Seminar4/src/main/se/kth/iv1350/posSystem/integration/ExternalInventorySystem.java
@@ -15,9 +15,12 @@ class ExternalInventorySystem {
         setStartingInventory();
     }
 
-    ItemDTO getItem(String itemID) throws InvalidItemIdentifierException {
-        if(!itemCatalogue.containsKey(itemID))
-            throw new InvalidItemIdentifierException(itemID);
+    ItemDTO getItem(String itemID) throws ItemIdentifierException, ExternalSystemException {
+        if (itemID.equals("404"))
+            throw new ExternalSystemException(this);
+
+        if (!itemCatalogue.containsKey(itemID))
+            throw new ItemIdentifierException(itemID);
 
         return this.itemCatalogue.get(itemID);
     }
@@ -52,6 +55,8 @@ class ExternalInventorySystem {
                 new Amount(23), new Amount(0.12)));
         this.itemCatalogue.put("32784623", new ItemDTO("32784623", "USB Wired Mouse",
                 new Amount(99), new Amount(0.25)));
+        this.itemCatalogue.put("404X", new ItemDTO("404X", "ConnectionFailure",
+                new Amount(0), new Amount(0)));
     }
 
     private void setStartingInventory() {
@@ -60,5 +65,6 @@ class ExternalInventorySystem {
         this.itemInventory.put("12312234", new Amount(32));
         this.itemInventory.put("67334553", new Amount(12));
         this.itemInventory.put("32784623", new Amount(3));
+        this.itemInventory.put("404X", new Amount(1));
     }
 }
