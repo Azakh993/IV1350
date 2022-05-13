120bf94a8bc38a8c12036b69663f57cad76e4c1a
Azmeer K <aaak@kth.se>
2022-05-13 13:25:09 +0200

    Added code for throwing ExternalSystemException when a specific item identifier signaling external system connection failure is last in basket.

--- Seminar4/src/main/se/kth/iv1350/posSystem/integration/ReceiptPrinter.java
+++ Seminar4/src/main/se/kth/iv1350/posSystem/integration/ReceiptPrinter.java
@@ -3,7 +3,9 @@ package se.kth.iv1350.posSystem.integration;
 import se.kth.iv1350.posSystem.model.SaleDTO;
 
 class ReceiptPrinter {
-    void printReceipt(SaleDTO saleDTO) {
+    void printReceipt(SaleDTO saleDTO) throws ExternalSystemException {
+        if(saleDTO.getLastRegisteredItem().getItemID().equals("404X"))
+            throw new ExternalSystemException(this);
         // Code here for printing receipt
     }
 }
