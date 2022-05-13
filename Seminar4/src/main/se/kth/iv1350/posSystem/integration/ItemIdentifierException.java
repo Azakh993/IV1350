b172c850d4948c4fef610dd47c132e11ff1a1549
Azmeer K <aaak@kth.se>
2022-05-13 10:16:26 +0200

    Renamed class.

--- Seminar4/src/main/se/kth/iv1350/posSystem/integration/ItemIdentifierException.java
+++ Seminar4/src/main/se/kth/iv1350/posSystem/integration/ItemIdentifierException.java
@@ -3,20 +3,22 @@ package se.kth.iv1350.posSystem.integration;
 /**
  * Thrown when an invalid item identifier is registered during a sale
  */
-public class InvalidItemIdentifierException extends Exception {
+public class ItemIdentifierException extends Exception {
     private final String invalidItemID;
 
     /**
      * Creates a new instance with an error message, which specifies which item identifier is invalid
+     *
      * @param invalidItemID The invalid item identifier.
      */
-    public InvalidItemIdentifierException(String invalidItemID) {
+    public ItemIdentifierException(String invalidItemID) {
         super("Item identifier '" + invalidItemID + "' not found in item catalogue!");
         this.invalidItemID = invalidItemID;
     }
 
     /**
      * Gets the invalid item identifier
+     *
      * @return The invalid item identifier
      */
     public String getInvalidItemID() {
