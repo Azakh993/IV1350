af47057aa2c99daa064b6ca3686c9c598784ff60
Azmeer K <aaak@kth.se>
2022-05-24 16:57:50 +0200

    Adjusted code for removed exception class.

--- HigherGradeTasks/src/test/se/kth/iv1350/posSystem/integration/CustomerDatabaseTest.java
+++ HigherGradeTasks/src/test/se/kth/iv1350/posSystem/integration/CustomerDatabaseTest.java
@@ -3,7 +3,6 @@ package se.kth.iv1350.posSystem.integration;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import se.kth.iv1350.posSystem.controller.Controller;
 
 import static org.junit.jupiter.api.Assertions.*;
 
