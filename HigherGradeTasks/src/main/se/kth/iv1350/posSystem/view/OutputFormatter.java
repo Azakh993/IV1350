7f41b7e6ac85b4eaaec863ade16d8a4938aabb74
Azmeer K <aaak@kth.se>
2022-05-24 16:22:22 +0200

    Repurposed for formatting of output from View.

---
+++ HigherGradeTasks/src/main/se/kth/iv1350/posSystem/view/OutputFormatter.java
@@ -0,0 +1,56 @@
+package se.kth.iv1350.posSystem.view;
+
+import se.kth.iv1350.posSystem.dto.BasketDTO;
+import se.kth.iv1350.posSystem.dto.ItemDTO;
+import se.kth.iv1350.posSystem.dto.ReceiptDTO;
+import se.kth.iv1350.posSystem.utilities.Amount;
+import se.kth.iv1350.posSystem.utilities.TimeAndDate;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+class OutputFormatter {
+
+	void startSalePrinter() {
+		System.out.println("\n\n[startSale()]\t\t\t\t|");
+	}
+
+	void addIemToBasketPrinter(BasketDTO basketDTO) {
+		ItemDTO lastRegisteredItem = basketDTO.getLastRegisteredItem();
+		String itemIDOfLastRegisteredItem = lastRegisteredItem.getItemID();
+		Amount qtyOfLastRegisteredItem = basketDTO.getBasket().get(lastRegisteredItem);
+		System.out.print("[addItem('" + itemIDOfLastRegisteredItem + "')]\t\t|\t");
+		System.out.print(String.format("%.0f", qtyOfLastRegisteredItem.getAmount()) + "* " + lastRegisteredItem);
+	}
+
+	void runningTotalPrinter(Amount runningTotal) {
+		System.out.printf("\t\t\t\t\t\t\t\t| Running total: " + runningTotal + " |\n");
+	}
+
+	void endSalePrinter(Amount totalPrice) {
+		System.out.print("[endSale()]\t\t\t\t\t|\t");
+		System.out.printf(String.format("Total Price (including VAT): %.2f\n", totalPrice.getAmount()));
+	}
+
+	void addDiscountsPrinter(Amount totalPriceAfterDiscount, String customerID) {
+		System.out.print("[addDiscount(" + customerID + ")]\t|\t");
+		System.out.printf(String.format("Total Price After Discount: %.2f\n", totalPriceAfterDiscount.getAmount()));
+	}
+
+	void registerPaymentPrinter(ReceiptDTO receiptDTO) {
+		System.out.print("[registerPayment(" + receiptDTO.getAmountPaid().getAmount() + ")]\t|\t");
+		System.out.println(String.format("Change: %.2f", receiptDTO.getChange().getAmount()));
+	}
+	String exceptionLogEntryFormatter(Exception exception) {
+		StringBuilder logEntry = new StringBuilder();
+		TimeAndDate timeAndDateOfException = new TimeAndDate();
+		logEntry.append(timeAndDateOfException.getTimeAndDate());
+		logEntry.append("\n");
+		StringWriter stackTraceAsString = new StringWriter();
+		PrintWriter printWriter = new PrintWriter(stackTraceAsString);
+		exception.printStackTrace(printWriter);
+		logEntry.append(stackTraceAsString);
+		logEntry.append("\n\n");
+		return logEntry.toString();
+	}
+}
