3e1744beba56b4297e42cbc73884ae832324b2f7
Azmeer K <aaak@kth.se>
2022-05-25 17:56:48 +0200

    Reformatted code.

--- Seminar5/src/test/se/kth/iv1350/posSystem/controller/ControllerTest.java
+++ Seminar5/src/test/se/kth/iv1350/posSystem/controller/ControllerTest.java
@@ -12,124 +12,126 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 class ControllerTest {
-    private Controller controller;
+	private Controller controller;
 
-    @BeforeEach
-    void setUp() {
-        this.controller = new Controller();
-    }
+	@BeforeEach
+	void setUp() {
+		this.controller = new Controller();
+	}
 
-    @AfterEach
-    void tearDown() {
-        this.controller = null;
-    }
+	@AfterEach
+	void tearDown() {
+		this.controller = null;
+	}
 
-    @Test
-    void testAddItem() {
-        this.controller.startSale();
-        String itemIDFromItemInventory = "32784623";
-        String expectedResult = itemIDFromItemInventory;
-        try {
-            BasketDTO basketDTO = this.controller.addItem(itemIDFromItemInventory);
-            String result = basketDTO.getLastRegisteredItem().getItemID();
-            assertEquals(expectedResult, result, "ReceiptDTO not generated correctly!");
-        } catch (ItemIdentifierException exception) {
-            fail("ItemIdentifierException incorrectly thrown!");
-        } catch (Exception exception) {
-            fail(exception.getClass() + "incorrectly thrown!");
-        }
-    }
+	@Test
+	void testAddItem() {
+		this.controller.startSale();
+		String itemIDFromItemInventory = "32784623";
+		String expectedResult = itemIDFromItemInventory;
+		try {
+			BasketDTO basketDTO = this.controller.addItem(itemIDFromItemInventory);
+			String result = basketDTO.getLastRegisteredItem().getItemID();
+			assertEquals(expectedResult, result, "ReceiptDTO not generated correctly!");
+		} catch (ItemIdentifierException exception) {
+			fail("ItemIdentifierException incorrectly thrown!");
+		} catch (Exception exception) {
+			fail(exception.getClass() + "incorrectly thrown!");
+		}
+	}
 
-    @Test
-    void testAddItemBeforeStartSale() {
-        String expectedResult = "Attempt to register item before starting a new sale.";
-        try {
-            this.controller.addItem("anyItemID");
-            fail("Item added before new sale initiated.");
-        } catch (ItemIdentifierException exception) {
-            fail("ItemIdentifierException incorrectly thrown!");
-        } catch (IllegalStateException exception) {
-            assertEquals(expectedResult, exception.getMessage(), "Exception message mismatch!");
-        }
-    }
+	@Test
+	void testAddItemBeforeStartSale() {
+		String expectedResult = "Attempt to register item before starting a new sale.";
+		try {
+			this.controller.addItem("anyItemID");
+			fail("Item added before new sale initiated.");
+		} catch (ItemIdentifierException exception) {
+			fail("ItemIdentifierException incorrectly thrown!");
+		} catch (IllegalStateException exception) {
+			assertEquals(expectedResult, exception.getMessage(), "Exception message mismatch!");
+		}
+	}
 
-    @Test
-    void testEndSaleBeforeStartingNewSale() {
-        String expectedResult = "Attempt to end sale before starting a new sale.";
-        try {
-            this.controller.endSale();
-            fail("Sale ended before initialization of a new sale.");
-        } catch (IllegalStateException exception) {
-            assertEquals(expectedResult, exception.getMessage(), "Exception message mismatch!");
-        }
-    }
+	@Test
+	void testEndSaleBeforeStartingNewSale() {
+		String expectedResult = "Attempt to end sale before starting a new sale.";
+		try {
+			this.controller.endSale();
+			fail("Sale ended before initialization of a new sale.");
+		} catch (IllegalStateException exception) {
+			assertEquals(expectedResult, exception.getMessage(), "Exception message mismatch!");
+		}
+	}
 
-    @Test
-    void testItemDiscountViaRequestDiscount() {
-        String validItemID = "67334553";
-        String validCustomerID = "2101012031";
-        Amount expectedResult = new Amount(299 * 2 - 299 * 2 * 0.2);
-        this.controller.startSale();
-        Amount result = new Amount(0);
-        try {
-            this.controller.addItem(validItemID);
-            this.controller.addItem(validItemID);
-        } catch (Exception exception) {
-            fail("Incorrectly thrown exception!");
-        }
-        this.controller.endSale();
-        try {
-            result = this.controller.requestDiscount(validCustomerID);
-        } catch (CustomerRegistrationException exception) {
-            fail("CustomerRegistrationException thrown incorrectly!");
-        }
-        assertEquals(expectedResult, result, "Item discount incorrectly calculated!");
-    }
+	@Test
+	void testItemDiscountViaRequestDiscount() {
+		String validItemID = "67334553";
+		String validCustomerID = "2101012031";
+		Amount expectedResult = new Amount(299 * 2 - 299 * 2 * 0.2);
+		this.controller.startSale();
+		Amount result = new Amount(0);
+		try {
+			this.controller.addItem(validItemID);
+			this.controller.addItem(validItemID);
+		} catch (Exception exception) {
+			fail("Incorrectly thrown exception!");
+		}
+		this.controller.endSale();
+		try {
+			result = this.controller.requestDiscount(validCustomerID);
+		} catch (CustomerRegistrationException exception) {
+			fail("CustomerRegistrationException thrown incorrectly!");
+		} catch (Exception exception) {
+			fail("Exception thrown incorrectly!");
+		}
+		assertEquals(expectedResult, result, "Item discount incorrectly calculated!");
+	}
 
-    @Test
-    void testTotalDiscountViaRequestDiscount() {
-        String validItemID = "95867956";
-        String validCustomerID = "2101012031";
-        Amount expectedResult = new Amount(1499 - 1499 * 0.05);
-        this.controller.startSale();
-        Amount result = new Amount(0);
-        try {
-            this.controller.addItem(validItemID);
-        } catch (Exception exception) {
-            fail("Incorrectly thrown exception!");
-        }
-        this.controller.endSale();
-        try {
-            result = this.controller.requestDiscount(validCustomerID);
-        } catch (CustomerRegistrationException exception) {
-            fail("CustomerRegistrationException thrown incorrectly!");
-        }
-        assertEquals(expectedResult, result, "Item discount incorrectly calculated!");
-    }
+	@Test
+	void testTotalDiscountViaRequestDiscount() {
+		String validItemID = "95867956";
+		String validCustomerID = "2101012031";
+		Amount expectedResult = new Amount(1499 - 1499 * 0.05);
+		this.controller.startSale();
+		Amount result = new Amount(0);
+		try {
+			this.controller.addItem(validItemID);
+		} catch (Exception exception) {
+			fail("Incorrectly thrown exception!");
+		}
+		this.controller.endSale();
+		try {
+			result = this.controller.requestDiscount(validCustomerID);
+		} catch (CustomerRegistrationException exception) {
+			fail("CustomerRegistrationException thrown incorrectly!");
+		}
+		assertEquals(expectedResult, result, "Item discount incorrectly calculated!");
+	}
 
-    @Test
-    void testRequestDiscountBeforeEndSale() {
-        this.controller.startSale();
-        String validCustomerID = "2101012031";
-        String expectedResult = "Attempt to request discount before starting ending sale.";
-        try {
-            this.controller.requestDiscount(validCustomerID);
-        } catch(CustomerRegistrationException exception) {
-            fail("CustomerRegistrationException thrown incorrectly!");
-        } catch (IllegalStateException exception) {
-            assertEquals(expectedResult, exception.getMessage(), "Exception message mismatch!");
-        }
-    }
+	@Test
+	void testRequestDiscountBeforeEndSale() {
+		this.controller.startSale();
+		String validCustomerID = "2101012031";
+		String expectedResult = "Attempt to request discount before starting ending sale.";
+		try {
+			this.controller.requestDiscount(validCustomerID);
+		} catch (CustomerRegistrationException exception) {
+			fail("CustomerRegistrationException thrown incorrectly!");
+		} catch (IllegalStateException exception) {
+			assertEquals(expectedResult, exception.getMessage(), "Exception message mismatch!");
+		}
+	}
 
-    @Test
-    void testRegisterPaymentBeforeEndSale() {
-        double amountPaidInCash = 100;
-        this.controller.startSale();
-        String expectedResult = "Attempt to register payment before ending sale.";
-        try {
-            this.controller.registerPayment(amountPaidInCash);
-        } catch (IllegalStateException exception) {
-            assertEquals(expectedResult, exception.getMessage(), "Exception message mismatch!");
-        }
-    }
+	@Test
+	void testRegisterPaymentBeforeEndSale() {
+		double amountPaidInCash = 100;
+		this.controller.startSale();
+		String expectedResult = "Attempt to register payment before ending sale.";
+		try {
+			this.controller.registerPayment(amountPaidInCash);
+		} catch (IllegalStateException exception) {
+			assertEquals(expectedResult, exception.getMessage(), "Exception message mismatch!");
+		}
+	}
 }
--- Seminar5/src/test/se/kth/iv1350/posSystem/integration/CustomerDatabaseTest.java
+++ Seminar5/src/test/se/kth/iv1350/posSystem/integration/CustomerDatabaseTest.java
@@ -4,31 +4,31 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 class CustomerDatabaseTest {
-    private CustomerDatabase customerDatabase;
+	private CustomerDatabase customerDatabase;
 
-    @BeforeEach
-    void setUp() {
-        this.customerDatabase = CustomerDatabase.getCustomerDB();
-    }
+	@BeforeEach
+	void setUp() {
+		this.customerDatabase = CustomerDatabase.getCustomerDB();
+	}
 
-    @AfterEach
-    void tearDown() {
-        this.customerDatabase = null;
-    }
+	@AfterEach
+	void tearDown() {
+		this.customerDatabase = null;
+	}
 
-    @Test
-    void testCustomerRegistrationException () {
-        String invalidCustomerID = "123";
-        String expectedResult = "CustomerID '" + invalidCustomerID + "' is unregistered!";
-        try {
-            this.customerDatabase.getCustomerDTO(invalidCustomerID);
-            fail("Retrieved non-existent customer information");
-        } catch (CustomerRegistrationException exception) {
-            assertEquals(expectedResult, exception.getMessage(), "Exception message mismatch!");
-        }
-
-    }
+	@Test
+	void testCustomerRegistrationException() {
+		String invalidCustomerID = "123";
+		String expectedResult = "CustomerID '" + invalidCustomerID + "' is unregistered!";
+		try {
+			this.customerDatabase.getCustomerDTO(invalidCustomerID);
+			fail("Retrieved non-existent customer information");
+		} catch (CustomerRegistrationException exception) {
+			assertEquals(expectedResult, exception.getMessage(), "Exception message mismatch!");
+		}
+	}
 }
