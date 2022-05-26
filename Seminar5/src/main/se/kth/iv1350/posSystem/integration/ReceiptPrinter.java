1343a8112e0f45abfbc62f6d514bfd5ae697585c
Azmeer K <aaak@kth.se>
2022-05-26 20:26:02 +0200

    Added code for showing functionality of adaptations of 'PrintWriter'.

--- Seminar5/src/main/se/kth/iv1350/posSystem/integration/ReceiptPrinter.java
+++ Seminar5/src/main/se/kth/iv1350/posSystem/integration/ReceiptPrinter.java
@@ -1,6 +1,10 @@
 package se.kth.iv1350.posSystem.integration;
 
 import se.kth.iv1350.posSystem.dto.ReceiptDTO;
+import se.kth.iv1350.posSystem.utilities.Task2.PrintWriterAdaptationViaComposition;
+import se.kth.iv1350.posSystem.utilities.Task2.PrintWriterAdaptationViaInheritance;
+
+import java.io.IOException;
 
 class ReceiptPrinter {
 	private static final ReceiptPrinter RECEIPT_PRINTER = new ReceiptPrinter();
@@ -10,6 +14,18 @@ class ReceiptPrinter {
 	}
 
 	void printReceipt(ReceiptDTO receiptDTO) {
-		System.out.println(receiptDTO.toString());
+		try {
+			PrintWriterAdaptationViaComposition compositionAdaptation = new PrintWriterAdaptationViaComposition();
+			compositionAdaptation.println("Entries are generated using an adaptation of PrintWriter;\n" +
+					"the method 'println() has been modified to start with a newline.");
+			compositionAdaptation.println(receiptDTO.toString());
+			PrintWriterAdaptationViaInheritance inheritanceAdaptation = new PrintWriterAdaptationViaInheritance();
+			inheritanceAdaptation.println("Entries are generated using an adaptation of PrintWriter;\n" +
+					"the method 'println() has been modified to start with a newline.");
+			inheritanceAdaptation.println(receiptDTO.toString());
+			inheritanceAdaptation.close();
+		} catch (IOException exception) {
+			exception.printStackTrace();
+		}
 	}
 }
