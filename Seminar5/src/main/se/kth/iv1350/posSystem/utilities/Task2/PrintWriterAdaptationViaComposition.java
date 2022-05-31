3eea22987792e9ecd905b0e39eadee6790b3fd14
Azmeer K <aaak@kth.se>
2022-05-31 16:57:18 +0200

    Added comments.

--- Seminar5/src/main/se/kth/iv1350/posSystem/utilities/Task2/PrintWriterAdaptationViaComposition.java
+++ Seminar5/src/main/se/kth/iv1350/posSystem/utilities/Task2/PrintWriterAdaptationViaComposition.java
@@ -4,14 +4,27 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+/**
+ * Adaptation of PrintWriter via composition; string printed using 'println' starts and ends
+ * with a newline.
+ */
 public class PrintWriterAdaptationViaComposition {
 	private final static String FILE_PATH = "Seminar5/textFiles/receiptViaCompositionAdaptation.txt";
 	private final PrintWriter printWriter;
 
+	/**
+	 * Creates a new class instance. Instantiates <code>PrintWriter</code> using <code>FileWriter</code>
+	 * and stores the reference.
+	 * @throws IOException If the file at specified <code>FILE_PATH</code> could not be created
+	 */
 	public PrintWriterAdaptationViaComposition() throws IOException{
 		printWriter = new PrintWriter(new FileWriter(FILE_PATH, true), true);
 	}
 
+	/**
+	 * Prints out a newline before and after <code>stringToPrint</code>
+	 * @param stringToPrint The string to printout
+	 */
 	public void println(String stringToPrint) {
 		printWriter.println("\n" + stringToPrint);
 	}
--- Seminar5/src/main/se/kth/iv1350/posSystem/utilities/Task2/PrintWriterAdaptationViaInheritance.java
+++ Seminar5/src/main/se/kth/iv1350/posSystem/utilities/Task2/PrintWriterAdaptationViaInheritance.java
@@ -4,14 +4,27 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+/**
+ * Adaptation of PrintWriter via inheritance; string printed using 'println' starts and ends
+ * with a newline.
+ */
 public class PrintWriterAdaptationViaInheritance extends PrintWriter {
 	private final static String FILE_PATH = "Seminar5/textFiles/receiptViaInheritanceAdaptation.txt";
 
-
+	/**
+	 * Creates a new class instance. Instantiates <code>PrintWriter</code> with <code>FileWriter</code>
+	 * using a super class constructor.
+	 * @throws IOException If the file at specified <code>FILE_PATH</code> could not be created
+	 */
 	public PrintWriterAdaptationViaInheritance() throws IOException{
 		super(new FileWriter(FILE_PATH, true), true);
 	}
 
+	/**
+	 * Overrides super class methpd 'println'; before and after printout of <code>stringToPrint</code>,
+	 * newline is entered.
+	 * @param stringToPrint the string to be printed
+	 */
 	@Override
 	public void println(String stringToPrint) {
 		super.println("\n" + stringToPrint);
